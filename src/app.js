c93cccd974cc444a830021db5f8c94228317691b
julianmolinerisit <julianmolinerisit@gmail.com>
2024-04-11 10:18:54 -0300

    Finish backend

--- src/app.js
+++ src/app.js
@@ -1,19 +1,42 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import taskRoutes from "./routes/tasks.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import { ORIGIN } from "./config.js";
+import { pool } from "./db.js";
 
 const app = express();
 
+// Middlewares
+app.use(
+  cors({
+    origin: ORIGIN,
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to my API" });
+// Routes
+app.get("/", (req, res) => res.json({ message: "welcome to my API" }));
+app.get("/api/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  return res.json(result.rows[0]);
 });
+app.use("/api", taskRoutes);
+app.use("/api", authRoutes);
 
-// Error handling middleware
+// Error Hander
 app.use((err, req, res, next) => {
-  res.status(500).json({ status: "error", message: err.message });
+  res.status(500).json({
+    status: "error",
+    message: err.message,
+  });
 });
 
 export default app;
