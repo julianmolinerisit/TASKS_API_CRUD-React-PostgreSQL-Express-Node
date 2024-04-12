f495374f74b6e929294fb19d5672c57984eec787
julianmolinerisit <julianmolinerisit@gmail.com>
2024-04-11 21:52:08 -0300

    First functional Version

--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,15 @@
-CREATE TABLE task(
+CREATE TABLE task(                                    -- Primero ejecutamos este
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) UNIQUE NOT NULL,
     description TEXT
 );
 
-ALTER TABLE task ADD COLUMN user_id INTEGER REFERENCES users(id);
+ALTER TABLE task ADD COLUMN user_id INTEGER REFERENCES users(id);    -- Tercero este
 
 -- remove unique from title
-ALTER TABLE task DROP CONSTRAINT task_title_key;
+ALTER TABLE task DROP CONSTRAINT task_title_key;                     -- Cuarto este
 
-CREATE TABLE users (
+CREATE TABLE users (                                    -- Segundo este
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -18,4 +18,4 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE users ADD COLUMN gravatar VARCHAR(255);
+ALTER TABLE users ADD COLUMN gravatar VARCHAR(255);   -- Quinto este
---
+++ frontend/src/App.jsx
@@ -0,0 +1,67 @@
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import { useAuth } from "./context/AuthContext";
+import { TaskProvider } from "./context/TaskContext";
+
+import Navbar from "./components/navbar/Navbar";
+import { Container } from "./components/ui";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TasksPage from "./pages/TasksPage";
+import TaskFormPage from "./pages/TaskFormPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  const { isAuth, loading } = useAuth();
+  console.log(loading)
+
+  if (loading) return <h1>
+    Cargando...
+  </h1>
+
+  return (
+    <>
+      <Navbar />
+
+      <Container className="py-5">
+        <Routes>
+          <Route
+            element={<ProtectedRoute isAllowed={!isAuth} redirectTo="/tasks" />}
+          >
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+
+          <Route
+            element={<ProtectedRoute isAllowed={isAuth} redirectTo="/login" />}
+          >
+            <Route
+              element={
+                <TaskProvider>
+                  <Outlet />
+                </TaskProvider>
+              }
+            >
+              <Route path="/tasks" element={<TasksPage />} />
+              <Route path="/tasks/new" element={<TaskFormPage />} />
+              <Route path="/tasks/:id/edit" element={<TaskFormPage />} />
+            </Route>
+
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
